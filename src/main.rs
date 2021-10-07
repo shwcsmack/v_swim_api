fbe72f92440e9d5daf8f1b5f18d0bcd6b2eaf3c6
Shayne Stewart <sh.wc.smack@gmail.com>
2021-10-06 23:05:08 -0500

    Added a health_check path and tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use v_swim_api::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    run(listener)?.await
 }
