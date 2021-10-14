c7f600dfe6e521eb9fcf2c0a926b46e0a6ed886d
Shayne Stewart <sh.wc.smack@gmail.com>
2021-10-13 22:19:05 -0500

    started adding telemetry to code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use v_swim_api::configuration::get_configuration;
 use v_swim_api::startup::run;
+use v_swim_api::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("v_swim".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
